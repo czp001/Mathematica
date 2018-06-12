e338c2392b60ead01ae64dae5a4f114407c9127c
czp001 <30425821+czp001@users.noreply.github.com>
2018-06-12 18:24:15 +0800

    Update 与抛物线有关的一种包络.m

--- 与抛物线有关的一种包络.m
+++ 与抛物线有关的一种包络.m
@@ -1,7 +1,8 @@
-f[k_] := Module[{},
+f[k_, m_] := 
+ Module[{}, 
   l = Table[{{y^2/4, y}, {y^2/4 + (2*k)/Sqrt[4 + y^2], 
       y - (y*k)/Sqrt[4 + y^2]}}, {y, -9, 9, 0.1}];
-  Show[Graphics[{Red, Line[#]} & /@ l, Axes -> True], 
-   ContourPlot[{y^2 == 4 x, (y + (y*k)/Sqrt[4 + y^2])^2 == 
-      4 (x - (2*k)/Sqrt[4 + y^2])}, {x, 0, 20}, {y, -10, 10}]]]
-f[8]
+  Show[ContourPlot[{y^2 == 4 x, (y + (y*k)/Sqrt[4 + y^2])^2 == 
+      4 (x - (2*k)/Sqrt[4 + y^2])}, {x, 0, 2 m}, {y, -m, m}], 
+   Graphics[{Red, Line[#]} & /@ l, Axes -> True]]]
+f[8, 10](*k为法线长度，m为图片的显示范围*)
