5d04ccfcab6b714bf1d4e355144f74b0b3145c55
czp001 <30425821+czp001@users.noreply.github.com>
2018-06-13 11:22:36 +0800

    Update 与抛物线有关的一种包络.m

--- 与抛物线有关的一种包络.m
+++ 与抛物线有关的一种包络.m
@@ -1,11 +1,21 @@
 f[k_] := Module[{},
   l = Table[{{y^2/4, y}, {y^2/4 + (2*k)/Sqrt[4 + y^2], 
-      y - (y*k)/Sqrt[4 + y^2]}}, {y, -11, 11, 0.1}];
-  Show[ContourPlot[{y^2 == 4 x, 
-     4 k^2 (4 + y^2)^3 == (4 x (4 + y^2) - 
-         y^2 (4 + y^2 + k^2))^2}, {x, -10, 30}, {y, -20, 20}], 
-   Graphics[{Red, Line[#]} & /@ l, Axes -> True]]](*k为法线长度*)
-f[1]
-f[2]
-f[5]
+      y - (y*k)/Sqrt[4 + y^2]}}, {y, -11, 11, 0.5}];
+  Show[ContourPlot[Evaluate@{y^2 == 4 x}, {x, -2, 30}, {y, -18, 18}, 
+    ContourStyle -> Blue], 
+   ParametricPlot[{t^2/4 + (2 k)/Sqrt[4 + t^2], 
+     t - (k*t)/Sqrt[4 + t^2]}, {t, -11, 11}, PlotStyle -> Yellow], 
+   Graphics[{Red, Line[#]} & /@ l, Axes -> True]]]
 f[8]
+lim = -(t /. 
+    Last@NMinimize[{t^2/4 + (2*8)/Sqrt[4 + t^2], -3 <= t <= 3}, t, 
+      WorkingPrecision -> 15])
+c[t_] := y + t/2 (x - t^2/4) - t;
+sol = {x, y} /. Solve[c[t] == 0 && D[c[t], t] == 0, {x, y}] // First
+ParametricPlot[{sol, {t^2/4 + (2*8)/Sqrt[4 + t^2], 
+   t - (8*t)/Sqrt[4 + t^2]}}, {t, -lim, lim}, 
+ PlotRange -> {{0, 10}, {-5, 5}}, PlotStyle -> Green]
+2 NIntegrate[
+  t^3/4 D[1/4 (8 + 3 t^2), t] + (t - (8*t)/Sqrt[4 + t^2]) D[
+     t^2/4 + (2*8)/Sqrt[4 + t^2], t], {t, 0, lim}, 
+  WorkingPrecision -> 15]
