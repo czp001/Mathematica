82bd0d1b886bff39d7a5c4b085f2604741318df2
czp001 <30425821+czp001@users.noreply.github.com>
2018-06-13 14:14:23 +0800

    Update 与抛物线有关的一种包络.m

--- 与抛物线有关的一种包络.m
+++ 与抛物线有关的一种包络.m
@@ -1,4 +1,4 @@
-f[k_] := Module[{},
+f[k_] := Module[{}, 
   l = Table[{{y^2/4, y}, {y^2/4 + (2*k)/Sqrt[4 + y^2], 
       y - (y*k)/Sqrt[4 + y^2]}}, {y, -11, 11, 0.5}];
   Show[ContourPlot[Evaluate@{y^2 == 4 x}, {x, -2, 30}, {y, -18, 18}, 
@@ -9,7 +9,7 @@ f[k_] := Module[{},
 f[8]
 lim = -(t /. 
     Last@NMinimize[{t^2/4 + (2*8)/Sqrt[4 + t^2], -3 <= t <= 3}, t, 
-      WorkingPrecision -> 15])
+      WorkingPrecision -> 30])
 c[t_] := y + t/2 (x - t^2/4) - t;
 sol = {x, y} /. Solve[c[t] == 0 && D[c[t], t] == 0, {x, y}] // First
 ParametricPlot[{sol, {t^2/4 + (2*8)/Sqrt[4 + t^2], 
@@ -18,4 +18,4 @@ ParametricPlot[{sol, {t^2/4 + (2*8)/Sqrt[4 + t^2],
 2 NIntegrate[
   t^3/4 D[1/4 (8 + 3 t^2), t] + (t - (8*t)/Sqrt[4 + t^2]) D[
      t^2/4 + (2*8)/Sqrt[4 + t^2], t], {t, 0, lim}, 
-  WorkingPrecision -> 15]
+  WorkingPrecision -> 30]
